a4651b2bb136f280b3c1155be96e1092f4b46220
Alexander Moore <31463017+moorea1@users.noreply.github.com>
2021-04-18 19:18:24 -0400

    Create train_test_split_maker.py

---
+++ train_test_split_maker.py
@@ -0,0 +1,17 @@
+# official train-test-split maker
+import numpy as np
+import random
+from sklearn.model_selection import train_test_split
+
+np.random.seed(1)
+random.seed(1)
+
+data = np.load('data/RESISC45_images_96.npy')
+labels = np.load('data/RESISC45_classes.npy')
+
+xtrain, xtest, ytrain, ytest = train_test_split(data, labels, test_size = .3)
+
+np.save('data/RESISC45_images_train.npy', xtrain)
+np.save('data/RESISC45_labels_train.npy', ytrain)
+np.save('data/RESISC45_images_test.npy', xtest)
+np.save('data/RESISC45_labels_test.npy', ytest)
