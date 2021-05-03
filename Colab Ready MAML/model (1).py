87f9558c989dd5738d318809de35bd9832185dfd
Vincent Filardi <vfilardi@wpi.edu>
2021-05-03 14:31:17 -0400

    Added Param called Quincy's model, when you set this to true it runs the other params computable with Quincy's model

---
+++ Colab Ready MAML/model (1).py
@@ -0,0 +1,54 @@
+import torch.nn as nn
+import utils
+from torchmeta.modules import (MetaModule, MetaSequential, MetaConv2d,
+                               MetaBatchNorm2d, MetaLinear)
+
+
+def conv3x3(in_channels, out_channels, **kwargs):
+    return MetaSequential(
+        MetaConv2d(in_channels, out_channels, kernel_size=3, padding=1, **kwargs),
+        MetaBatchNorm2d(out_channels, momentum=1., track_running_stats=False),
+        nn.ReLU(),
+        nn.MaxPool2d(2)
+    )
+
+class ConvolutionalNeuralNetwork(MetaModule):
+    def __init__(self,Params,in_channels=3,out_features=45,hidden_size=2):
+        super(ConvolutionalNeuralNetwork, self).__init__()
+        self.in_channels = Params['in_channels']
+        self.out_features = Params['num_classes']
+        self.hidden_size = Params['hidden_size']
+
+        self.features  = MetaSequential(
+            MetaConv2d(in_channels,hidden_size**5,kernel_size=4,stride=2,padding=1),
+            nn.ReLU(),
+            MetaConv2d(hidden_size**5,hidden_size**6,kernel_size=4,stride=2,padding=1,bias=False),
+            MetaBatchNorm2d(hidden_size**6),
+            nn.ReLU(),
+            MetaConv2d(hidden_size ** 6, hidden_size ** 7, kernel_size=4, stride=2, padding=1, bias=False),
+            MetaBatchNorm2d(hidden_size**7),
+            nn.ReLU(),
+            MetaConv2d(hidden_size ** 7, hidden_size ** 8, kernel_size=4, stride=2, padding=1, bias=False),
+            MetaBatchNorm2d(hidden_size ** 8),
+            nn.ReLU(),
+            MetaConv2d(hidden_size ** 8, hidden_size ** 9, kernel_size=4, stride=2, padding=1, bias=False),
+            MetaBatchNorm2d(hidden_size ** 9),
+            nn.ReLU(),
+            MetaConv2d(hidden_size ** 9, hidden_size ** 10, kernel_size=4, stride=2, padding=1, bias=False),
+            MetaBatchNorm2d(hidden_size ** 10),
+            nn.ReLU(),
+        )
+        self.classifier = MetaLinear(self.hidden_size**10, out_features)
+
+
+
+    def weight_init(self):
+        for m in self.features:
+            utils.normal_init(self._modules[m])
+    def forward(self, inputs, params=None):
+        features = self.features(inputs, params=self.get_subdict(params, 'features'))
+        features = features.view((features.size(0), -1))
+        logits = self.classifier(features, params=self.get_subdict(params, 'classifier'))
+        return logits
+
+
--- Colab Ready MAML/utils.py
+++ Colab Ready MAML/utils.py
@@ -17,12 +17,19 @@ def taskStructure(dataset,Params,train_labels):
     for i in range(45):
         labels[i] = [j for j, x in enumerate(train_labels) if x == i]
     metaDataset = l2l.data.MetaDataset(trainDATA,labels_to_indices=labels)
-    transforms = [
+    if Params['Quincy'] == False:
+        transforms = [
         ## need kshots*2 for inner and outer loop split accross each task
-        l2l.data.transforms.FusedNWaysKShots(metaDataset,n=Params['nways'],k=Params['kshots']*(Params['outerVSinner']+1),replacement=False),
-        l2l.data.transforms.LoadData(metaDataset),
-        l2l.data.transforms.ConsecutiveLabels(metaDataset)
-    ]
+            l2l.data.transforms.FusedNWaysKShots(metaDataset,n=Params['nways'],k=Params['kshots']*(Params['outerVSinner']+1),replacement=False),
+            l2l.data.transforms.LoadData(metaDataset),
+            l2l.data.transforms.ConsecutiveLabels(metaDataset)
+        ]
+    else:
+        print("doing Quincy's model")
+        train_loader = torch.utils.data.DataLoader(dataset, batch_size=int(Params['bs']*2), shuffle=True) #BUG: must keep shuffle false - or else it screws up labels, apparently
+        return train_loader
+
+
     taskset = l2l.data.TaskDataset(metaDataset,
                                    transforms,
                                    num_tasks=math.floor(Params['trainsz']/((Params['nways']*Params['kshots']*(Params['outerVSinner']+1))))
@@ -83,7 +90,7 @@ def get_data(Params,load=True):
     if load:
         file = '/content/drive/MyDrive/Project/data/RESISC45_images_96.npy'
         data = (np.load(file)/127.5)-1
-
+        print(data.shape)
         print(data.shape)
         labels = np.load('/content/drive/MyDrive/Project/data/RESISC45_classes.npy')
         test_size = 0.25
@@ -198,6 +205,9 @@ def colabgetdata(Params,load=False):
     img_size = train_data.shape[2]  # can use this to mofidy data size to fit this model (which only takes 256 images)
     c_dim = classes.shape[0]
     xtrain, xval, ytrain, yval = train_test_split(train_data, train_labels, test_size=0.25)
+    Params["trainsz"] = len(xtrain)
+    bs=32
+    Params['bs'] = bs
 
     xtrain = torch.tensor(xtrain).permute(0, 3, 1, 2)
     trainset = linkDataset(xtrain,ytrain)
@@ -222,9 +232,7 @@ def colabgetdata(Params,load=False):
     test_loader = torch.utils.data.DataLoader(testset, batch_size=512, drop_last=True,
                                               shuffle=False) # BUG: must keep shuffle false - or else it screws up labels, apparently
     Params['Img_size'] = img_size
-    Params['bs'] = bs
     Params["c_dim"] = c_dim
-    Params["trainsz"] = len(xtrain)
 
 
     return train_tasks,val_loader,test_loader, Params
